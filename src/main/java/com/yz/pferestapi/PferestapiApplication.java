8c9bb766723052830f6f29eb365ad4bd23d9263e
Yassine Zribi <yassinezribiyassine@gmail.com>
2024-03-22 17:32:48 +0100

    Initial commit

---
+++ src/main/java/com/yz/pferestapi/PferestapiApplication.java
@@ -0,0 +1,13 @@
+package com.yz.pferestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PferestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PferestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yz/pferestapi/PferestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yz.pferestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PferestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
