eb331caffaf4bf1d2c78d614c03205ba735df247
Yassine Zribi <yassinezribiyassine@gmail.com>
2024-04-01 16:15:54 +0200

    Remove the register functionality

--- src/main/java/com/yz/pferestapi/controller/AuthController.java
+++ src/main/java/com/yz/pferestapi/controller/AuthController.java
@@ -2,11 +2,8 @@ package com.yz.pferestapi.controller;
 
 import com.yz.pferestapi.dto.LoginDto;
 import com.yz.pferestapi.dto.LoginResponseDto;
-import com.yz.pferestapi.dto.RegisterDto;
-import com.yz.pferestapi.entity.User;
 import com.yz.pferestapi.service.AuthService;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -22,12 +19,6 @@ public class AuthController {
 
     public AuthController(AuthService authService) {
         this.authService = authService;
-    }
-
-    @PostMapping("/register")
-    public ResponseEntity<User> register(@Valid @RequestBody RegisterDto registerDto) {
-        User registeredUser = authService.register(registerDto);
-        return new ResponseEntity<>(registeredUser, HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
--- src/main/java/com/yz/pferestapi/service/AuthService.java
+++ src/main/java/com/yz/pferestapi/service/AuthService.java
@@ -2,23 +2,16 @@ package com.yz.pferestapi.service;
 
 import com.yz.pferestapi.dto.LoginDto;
 import com.yz.pferestapi.dto.LoginResponseDto;
-import com.yz.pferestapi.dto.RegisterDto;
-import com.yz.pferestapi.entity.Role;
-import com.yz.pferestapi.entity.RoleEnum;
 import com.yz.pferestapi.entity.User;
-import com.yz.pferestapi.exception.AppException;
 import com.yz.pferestapi.repository.RoleRepository;
 import com.yz.pferestapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -33,27 +26,6 @@ public class AuthService {
     private final AuthenticationManager authenticationManager;
 
     private final JwtService jwtService;
-
-
-    public User register(RegisterDto registerDto) {
-
-        if (userRepository.existsByEmail(registerDto.getEmail())){
-            throw new AppException(HttpStatus.CONFLICT, "Email already exists!");
-        }
-
-        Role optionalRole = roleRepository.findByName(RoleEnum.USER)
-                .orElseThrow(() -> new AppException(HttpStatus.NOT_FOUND, "Role not found"));
-
-        User user = User.builder()
-                .firstName(registerDto.getFirstName())
-                .lastName(registerDto.getLastName())
-                .email(registerDto.getEmail())
-                .password(passwordEncoder.encode(registerDto.getPassword()))
-                .roles(List.of(optionalRole))
-                .build();
-
-        return userRepository.save(user);
-    }
 
     public LoginResponseDto login(LoginDto loginDto) {
         Authentication authentication = authenticationManager.authenticate(
