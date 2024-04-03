b878f2d827eff74ef1d617640b4c9d64bedb3526
Yassine Zribi <yassinezribiyassine@gmail.com>
2024-04-03 11:27:42 +0200

    Handle serve static files of the '/uploads' folder

---
+++ src/main/java/com/yz/pferestapi/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.yz.pferestapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS));
+    }
+}
--- src/main/java/com/yz/pferestapi/security/SecurityConfig.java
+++ src/main/java/com/yz/pferestapi/security/SecurityConfig.java
@@ -84,7 +84,7 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
-                .authorizeHttpRequests(ar -> ar.requestMatchers("/auth/login", "/auth/register").permitAll())
+                .authorizeHttpRequests(ar -> ar.requestMatchers("/auth/login", "/uploads/**").permitAll())
                 .authorizeHttpRequests(ar -> ar.anyRequest().authenticated())
                 //.httpBasic(Customizer.withDefaults())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())
