120908a27f0adf043c6312787f58a09ae3271bbc
Yassine Zribi <yassinezribiyassine@gmail.com>
2025-01-21 12:18:02 +0100

    Setting up an EmailService with no implementation yet

---
+++ src/main/java/com/yz/pferestapi/service/EmailService.java
@@ -0,0 +1,12 @@
+package com.yz.pferestapi.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    public void send(String to, String subject, String text) {
+        // TODO
+    }
+}
